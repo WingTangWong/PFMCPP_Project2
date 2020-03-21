6023e436a3440749d4815e7219853cb93023f476
WingTangWong <wingedpower@gmail.com>
2020-03-21 01:03:32 -0700

    Added functions and the ignoreunused for their vars.

--- main.cpp
+++ main.cpp
@@ -48,6 +48,7 @@ video: Chapter 2 - Part 3
     if your function returns something other than void, add 'return {};' at the end of it.
   
 
+   * Done below in the function 
 
 
 4) provide default values for an arbitrary number of parameters in the function parameter list.
@@ -97,9 +98,9 @@ void variableDeclarations()
     bool   isItemFull        = false;
 
     // Floating Point
-    float  realPiValue     = 3.14159;
-    float  realRepeatValue = 1.18181;   // (13.0/11.0) = 1.18181818181818181818
-    float  realPercentage  = 0.158673;  // (1234/7777) = 0.15867301015815867301
+    float  realPiValue     = 3.14159f;
+    float  realRepeatValue = 1.18181f;   // (13.0/11.0) = 1.18181818181818181818
+    float  realPercentage  = 0.15867f;  // (1234/7777) = 0.15867301015815867301
 
     // Double Floating Point
     double longerPiValue     = 3.14159265358979323; 
@@ -137,72 +138,155 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 } 
 
 /*
- 1)
+ 1) Integer 
  */
 
-/*
- 2)
- */
+int getCountOfCarsRented( int rentalLocation=0, int carType = 0)
+{
+   ignoreUnused(rentalLocation, carType);
+   return {};
+}
+
+
 
 /*
- 3)
+ 2) Character 
  */
 
-/*
- 4)
- */
+char getGenderOfRenter( int rentalLocation=0, int rentalReservationNumber=0)
+{
+   ignoreUnused(rentalLocation, rentalReservationNumber);
+   return {};
+}
 
 /*
- 5)
+ 3) Boolean
  */
 
-/*
- 6)
- */
+bool isCarRented( int rentalLocation=0, int rentalCarID=0, float rentalRemainingTime=0 )
+{
+   ignoreUnused(rentalLocation, rentalCarID, rentalRemainingTime);
+   return {};
+}
+
 
 /*
- 7)
+ 4) Floating Point
  */
 
-/*
- 8)
- */
+float rentalTimeRemaining( int rentalLocation=0, int rentalReservationNumber=0)
+{
+   ignoreUnused(rentalLocation, rentalReservationNumber);
+   return {};
+}
+
 
 /*
- 9)
+ 5) Double Floating Point
  */
+
+double rentalTimeTally( int rentalLocation=0, bool useFilters=false, int filterID=0)
+{
+   ignoreUnused(rentalLocation, useFilters, filterID);
+   return {};
+}
+
+
+/*
+ 6) Void
+ */
+
+void queueReportGeneration( int rentalLocation=0, double startTime=0.00, double endTime=0.00 )
+{
+   ignoreUnused(rentalLocation, startTime, endTime);
+}
+
+
+/*
+ 7) int
+ */
+
+int getReportQueueCount( int rentalLocation=0)
+{
+   ignoreUnused(rentalLocation);
+   return {};
+}
+
+
+/*
+ 8) int
+ */
+
+int registerNewCar( int rentalLocation=0, int carType=0, float carAge=0.00, char carColor='b', double carMilleage=0.00 )
+{
+   ignoreUnused(rentalLocation, carType, carAge, carColor, carMilleage);
+   return {};
+}
+
+/*
+ 9) double
+ */
+
+double carWeight( int rentalLocation=0, int carID=0)
+{
+   ignoreUnused(rentalLocation, carID);
+   return {};
+}
 
 /*
  10)
  */
 
+char rentalExperienceRating( int rentalLocation=0, int carID=0, int rentalReservationNumber=0)
+{
+   ignoreUnused(rentalLocation,carID,rentalReservationNumber);
+   return {};
+}
+
+
+
 int main()
 {
     //example of calling that function, storing the value, and passing it to ignoreUnused at the end of main()
-    auto carRented = rentACar(6, 2); 
+      auto carRented = rentACar(6, 2); 
     
-    //1)
-    
-    //2)
-    
-    //3)
-    
-    //4)
-    
-    //5)
-    
-    //6)
-    
-    //7)
-    
-    //8)
-    
-    //9)
-    
-    //10)
-    
+    //1) int getCountOfCarsRented( int rentalLocation=0, int carType = 0)
+      auto numberOfCarsRented = getCountOfCarsRented( 42, 6);
+
+    //2) char getGenderOfRenter( int rentalLocation=0, int rentalReservationNumber=0)
+      auto thatRentersGender = getGenderOfRenter( 3011, 12342);
+
+    //3) bool isCarRented( int rentalLocation=0, int rentalCarID=0, float rentalRemainingTime=0 )
+      auto doTheyHaveMyTesla = isCarRented( 913, 11399, 3.331f);
+
+    //4) float rentalTimeRemaining( int rentalLocation=0, int rentalReservationNumber=0)
+      auto needMoreTimeOrNot = rentalTimeRemaining( 1434, 3112);
+
+    //5) double rentalTimeTally( int rentalLocation=0, bool useFilters=false, int filterID=0)
+      auto timeOfRentedCars = rentalTimeTally( 917, true, 17);
+
+    //6) void queueReportGeneration( int rentalLocation=0, double startTime=0.00, double endTime=0.00 )
+      queueReportGeneration( 777, 12123122.4413123,12123999.0);
+
+    //7) int getReportQueueCount( int rentalLocation=0)
+      auto reportBackLog = getReportQueueCount( 777 );
+
+    //8) int registerNewCar( int rentalLocation=0, int carType=0, float carAge=0.00, char carColor='b', double carMilleage=0.00 )
+      auto registedCarID = registerNewCar( 777, 9, 413.31f, 'r', 313319.31314);
+
+    //9) double carWeight( int rentalLocation=0, int carID=0)
+      auto carWeightInGramsWhy = carWeight( 777, 23241);
+
+    //10) char rentalExperienceRating( int rentalLocation=0, int carID=0, int rentalReservationNumber=0)
+      auto howDidWeDoThisTime = rentalExperienceRating( 777, 23241, 12342);
+
     
     ignoreUnused(carRented);
+    ignoreUnused(howDidWeDoThisTime,carWeightInGramsWhy,registedCarID,reportBackLog,timeOfRentedCars,doTheyHaveMyTesla,needMoreTimeOrNot,thatRentersGender,numberOfCarsRented);
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
+
+
+
+
